14b8122fcd383cb2e365742934a8668a12b3e078
Valentain2345 <valentino_ru@outlook.es>
2025-10-11 01:18:36 -0300

    Modifica readme

--- spring/src/main/java/org/aplication/logicaGrafo/query/SparqlQueryExecutor.java
+++
@@ -1,9 +0,0 @@
-package org.logicaGrafo.query;
-
-import org.apache.jena.query.Dataset;
-import org.apache.jena.query.Query;
-import org.logicaGrafo.SparqlQueryResult;
-
-public interface SparqlQueryExecutor {
-	SparqlQueryResult execute(Query query, Dataset dataset);
-}
